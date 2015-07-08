a4d1b594ac1ea8874206fa2e9e2a157128ace80e
ryan.h <ryan.h@daumkakao.com>
2015-07-08 19:15:18 +0900

    태스 수정중

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,9 @@
 class Post < ActiveRecord::Base
-  validates :name,  :presence => true
+  validates :name, :presence => true
   validates :title, :presence => true,
-            :length => { :minimum => 5 }
+            :length => {:minimum => 5}
 
-  has_many  :comments, :dependent => :destroy
+  has_many :comments, :dependent => :destroy
   has_many :tags
 
   accepts_nested_attributes_for :tags, :allow_destroy => :true,
